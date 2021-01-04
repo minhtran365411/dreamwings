c85a5533b6e7bec52dc062bde8b9ead6c98766a2
Minh Tran <minhtran344@gmail.com>
2021-01-04 15:54:45 +0700

    fix

--- app.js
+++ app.js
@@ -482,12 +482,12 @@ app.post("/thong-tin-lien-lac", function(req, res) {
 	    transporter.sendMail(mailOptions, function(err, data) {
 	        if (err) {
 	            console.log(err);
-		    	console.log(mailOpts);
+		    	console.log(mailOptions);
 			      res.render('contact-failure', {url: currenturl}) // Show a page indicating failure
 			      // window.alert('Gửi thư thất bại, xin thử lại sau hoặc liên hệ số điện thoại: 0903674268.');
 	        } else {
 	             // window.alert('Đã gửi thư thành công! Chúng tôi sẽ liên hệ với bạn sớm nhất có thể.');
-			    console.log(mailOpts);
+			    console.log(mailOptions);
 			      res.render('contact-success', {url: currenturl}) // Show a page indicating success
 			      
 		        }
